10bae0c5608fbd3ec1060b9dfb6d36cd25c8c63f
laisgiardullo <laisgiardullo@gmail.com>
2017-11-22 15:19:44 -0200

    mudanca_banco

--- gerar_banco.py
+++ gerar_banco.py
@@ -12,5 +12,6 @@ with con:
     cur.execute("CREATE TABLE Pessoa(Id INT, Status TEXT, Width INT, Instante_Inicial INT, Instante_Saida INT)")
     cur.execute("CREATE TABLE Posicao(Id INTEGER PRIMARY KEY AUTOINCREMENT, X REAL, Y REAL, Instante_Inicial INT, Instante_Final INT, Atual INT, Pessoa_id INT, FOREIGN KEY(Pessoa_id) REFERENCES Pessoa(Id))")
     cur.execute("CREATE TABLE Local(Id INTEGER PRIMARY KEY AUTOINCREMENT, Tipo TEXT, X INT, Y INT, Width INT, Height INT)")
+    cur.execute("CREATE TABLE Medidas(Id INTEGER PRIMARY KEY AUTOINCREMENT, X INT, Y INT, Width INT, Height INT)")
     #cur.execute("INSERT INTO Pessoa VALUES(1,'Audi',52642)")
 con.close
